8d63184bab7e0e90f5c0e1ddae4a6864f7d215ad
alunolages <alunolages@lages.sc>
2024-03-22 08:43:36 -0300

    teste

---
+++ kotlin/1tipos.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val a: Int = 10000
+    val d: Double = 100.00
+    val f: Float = 100.00f
+    val l: Long = 1000000004
+    val s: Short = 10
+    val b: Byte = 1
+    
+    println("Seu valor int é "+a)
+    println("Seu valor double é "+d)
+    println("Seu valor float é "+f)
+    println("Seu valor long é "+l)
+    println("Seu valor short é "+s)
+    println("Seu valor byte é "+b)
+}
