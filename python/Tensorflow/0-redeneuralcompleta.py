4466a9240c9b77abef6e287aa5e278ef8621aa91
alunolages <alunolages@lages.sc>
2024-04-12 10:28:08 -0300

    exclusão do projeto angular (que nao foi criado corretamente) e inclusão de exercicio do tensorflow

--- python/Tensorflow/0-redeneuralcompleta.py
+++ python/Tensorflow/0-redeneuralcompleta.py
@@ -1,22 +1,26 @@
+# Nesse codigo criamos, treinamos e extraimos parametros de uma rede neural
+
 # import do Tensorflow e tf.keras
 import tensorflow as tf
 from tensorflow import keras
 
 # Bibliotecas auxiliares
 import numpy as np
-import matplotlib.pyplu as plt
+import matplotlib.pyplot as plt
 
 # Printa a versão pra ver se o TF ta instalado
 print(tf.__version__)
 
-# Importanto a base de dados
+# Importanto a base de dados (usada para criar e testar a rede neural)
 fashion_mnist = keras.datasets.fashion_mnist
 
+# Cria o array de treinamento (imagens e rotulos para dizer o que cada imagem é)
+# e cria o array de teste e seus rotulos
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 # pra mostra a forma do array
 # saida: 60k imagens, 28x28
-train_images.shape
+print(train_images.shape)
 
 # pra inspecionar as imagens
 plt.figure() # cria uma figura
@@ -26,8 +30,13 @@ plt.grid(False)
 plt.show() # mostra a imagem
 
 # pra efeito computacional é melhor tranformar as cores pra ficarem numa escala de 0 a 1
+# divide todas as imagens do array por 255.0 (itera o array - recurso do numpy)
 train_images = train_images / 255.0
 test_images = test_images / 255.0
+
+# Definindo os nomes das classes (rótulos)
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 # mostrando 25 fotos
 plt.figure(figsize=(10,10))
@@ -36,7 +45,7 @@ for i in range(25):
     plt.xticks([])
     plt.yticks([])
     plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.com.binary)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i]])
 plt.show()
 
@@ -58,6 +67,7 @@ model.fit(train_images, train_labels, epochs=10)
 # avaliando a acurácia
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 print('\nTest accuracy:', test_acc)
+print('\nTeste Losss', test_loss)
 
 # predições
 predictions = model.predict(test_images)
@@ -67,26 +77,32 @@ test_labels[0]
 
 # se der acerto ele vai da azul, se der errado vai da vermelho
 def plot_image(i, predictions_array, true_label, img):
+    # extrai os dados para aquele elemento (i)
     predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    # limpa o grafico plotado (pra nao ficar linhas sobrepostas)
     plt.grid(False)
     plt.xticks([])
     plt.yticks([])
     
+    # exibe a imagem em cinza (cmap = mapa de cores)
     plt.imshow(img, cmap=plt.cm.binary)
 
+    # pega o valor mais alto do vetor de previsões (o que ele acha que é)
+    # se estiver correto conforme a label, ele pinta de azul senão vermelho
     predicted_label = np.argmax(predictions_array)
     if predicted_label == true_label:
         color = 'blue'
     else:
         color = 'red'
 
+    # plota no grafico
     plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label], 100*np.max(predictions_array), class_names[true_label]), color=color)
 
 # Faz o desenho do azul e vermelho em um grafico
 def plot_value_array(i, predictions_array, true_label):
     predictions_array, true_label = predictions_array[i], true_label[i]
     plt.grid(False)
-    plt.xticks([])
+    plt.xticks(range(10), class_names, rotation=90)
     plt.yticks([])
     thisplot = plt.bar(range(10), predictions_array, color="#777777")
     plt.ylim([0, 1])
